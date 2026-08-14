ceec7dd5fe7d08e8ab24057ff2b17eecf3bb51e2
ebrar türüdü <ebrartrd@gmail.com>
2026-08-15 00:28:11 +0300

    chore: initialize project infrastructure

---
+++ src/main/java/com/projecthub/projecthub_api/ProjecthubApiApplication.java
@@ -0,0 +1,13 @@
+package com.projecthub.projecthub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecthubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecthubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projecthub/projecthub_api/ProjecthubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecthub.projecthub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecthubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
